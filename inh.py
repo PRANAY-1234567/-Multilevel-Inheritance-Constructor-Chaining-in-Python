3c806d779b5bc25ed6bcc800454816be4521c046
Pranay <pranayvjadhao197@gmail.com>
2026-05-18 17:19:17 +0530

    Initial commit

---
+++ inh.py
@@ -0,0 +1,17 @@
+class Base:
+    def __init__(self):
+        print("Inside class Base default constructor");
+
+class Derived(Base):
+    def __init__(self):
+        super().__init__()
+        print("Inside class Derived default constructor")
+
+class Derived1(Derived):
+    def __init__(self):
+        super().__init__()
+        print("Inside class Derived1 default constructor")
+
+
+if __name__ == "__main__":
+    obj = Derived1()
