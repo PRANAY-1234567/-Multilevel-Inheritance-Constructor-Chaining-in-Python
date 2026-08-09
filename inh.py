3cc74ee455dcab9dab0609bdfc0fae8e17070f0d
Pranay Jadhao <pranayvjadhao197@gmail.com>
2026-08-09 23:15:30 +0530

    Update inh.py

--- inh.py
+++ inh.py
@@ -7,6 +7,7 @@ class Derived(Base):
         super().__init__()
         print("Inside class Derived default constructor")
 
+
 class Derived1(Derived):
     def __init__(self):
         super().__init__()
